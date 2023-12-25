2356ffba75a01ce280ab13599a2f316dd4efafa1
yahya-s9 <yahya.qt@berkeley.edu>
2023-12-25 13:05:48 -0800

    event handler

--- react_tutorial_app/src/App.tsx
+++ react_tutorial_app/src/App.tsx
@@ -1,8 +1,11 @@
 import ListGroup from './components/ListGroup';
 
-function App () {
-  return <div><ListGroup/></div>;
+// we get errors if we use the word class, bc it's
+// a reserved word in JS or TS. So replace class with className
 
+// putting ur JSX in paranthesese allows u to spread it over multuple lines
+function App () {
+  return <ListGroup/>
 }
 
 export default App
--- react_tutorial_app/src/components/ListGroup.tsx
+++ react_tutorial_app/src/components/ListGroup.tsx
@@ -1,5 +1,55 @@
+// import { Fragment } from "react";
+// <></> below works the same as <Fragment><Fragment/>
+
+import { MouseEvent } from "react";
+ 
 function ListGroup () {
-    return <h1>List Group</h1>;
+
+    const items = ['Marrakech', 'Casablanca', 'Rabat'];
+
+    const getMessage = () => {
+        // return items.length === 0 ? <p> No items to render </p> : null
+        return items.length === 0 && <p> No items to render </p>
+    }
+
+
+    // Event handler 
+
+    // used type annotaation syntax (event: MouseEvent) to specify
+    // to the TS compiler that event is of type mouseEvent
+    const handleClick = (event: MouseEvent) => {
+        // now that compiler knows what type of object event is,
+        // we can use the dot operator to get any property we want
+        // off the event object
+        console.log(event);
+    }
+    
+    return ( 
+        // fragment so we can render multiple elements without having to
+        // wrap everything around an unnecesary div
+        <>
+            <h1>Matesha</h1>
+            {/* to accomplish conditional rendering, cant write an if statement so 
+            instead have a JS expression embedded with curly braces that will
+            return the p tag above if the item list is empty */}
+            {getMessage}
+            <ul className="list-group">
+                {/* mapping every item in the items array to a corresponding
+                list item element, with a key that is also equal to the string in
+                the items array, and an on click property that has a function which prints the item when clicked */}
+                {items.map((item,index) => (
+                <li className="list-group-item" 
+                key={item} 
+                // dont call the function, just pass it in bc 
+                // its going to be called llater, at run time.
+                onClick={handleClick}
+                >
+                    {item}
+                </li>
+            ))}
+            </ul>
+        </>
+        );
 }
 
 export default ListGroup;
